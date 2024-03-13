c7548c741731ab6a8ccb95799e22280cd8a6373b
TheStrifIsRife <55000539+Bjburn2689@users.noreply.github.com>
2024-03-13 14:39:17 -0400

    Added some things for account creation

--- src/ung_wishlist/Authentication.java
+++ src/ung_wishlist/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication {
 	// DBconnection password
 	
 	
-	public static boolean authenticate (String username, String password) {
+	public static boolean authenticate (String username) {
 		// connect to db
 		// search db for username using SQL
 		// if found search for password using SQL
@@ -15,7 +15,28 @@ public class Authentication {
 		// else return false if
 		
 		
-	return false; //placeholder
+		return false; //placeholder
+	}
+
+	public static boolean checkUsernameExists (String username) {
+		//When creating new account, check if username is already take
+		//connect to db
+		//search db for username using SQL
+		//if found return true
+		//if not found return false (username not already taken, can be used)
+		
+		return false; //placeholder
+	}
+
+	public static boolean checkEmailExists(String email){
+		//When creating new account, check if there is already an account associated with an email address
+		//connect to db
+		//search db for email using SQL
+		//if found return true
+		//if not found return false (email not associated with account, can be used)
+
+
+		return false; //placeholder
 	}
 	
 	public static void main(String[] args) {
